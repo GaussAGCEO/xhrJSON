02863d4b9f710f3a7220fe3057b692996c343652
Alexander Gauss <46023039+GaussAGCEO@users.noreply.github.com>
2019-07-05 10:18:18 +0200

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -4,7 +4,7 @@ function xhrJSON(Alldata, callback){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            callback(this.responseText);
+            callback(JSON.parse(this.responseText));
         }
     };
     xhttp.open("POST", url+"?"+encodeJSONForURIparameters(data), true);
